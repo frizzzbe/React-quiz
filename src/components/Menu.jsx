c446c9a252da30e46aa5f802463fa4a0c755405d
Arkadiy <frizzzbe@yandex.ru>
2023-04-04 18:36:57 +0300

    Test questions add

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setTestId } from "../redux/slices/questionsSlice";
+import tests from "../assets/tests";
 
-// import App from './App';
-// {/* <App testId={1}/> */}
+// {/* <li onClick={()=>selectTest(0)} className="menu-item">Тест на знание Frontend</li>
+// <li onClick={()=>selectTest(1)} className="menu-item">Тест на знание React</li>
+// <li onClick={()=>selectTest(2)} className="menu-item">Интересные вопросы про автомобили</li>
+// <li onClick={()=>selectTest(3)} className="menu-item">Насколько хорошо, ты знаешь кинематограф</li>
+// <li onClick={()=>selectTest(4)} className="menu-item">Тест на историю</li>
+// <li onClick={()=>selectTest(5)} className="menu-item">Тест по основам школьной физики</li>
+// <li onClick={()=>selectTest(6)} className="menu-item">Как хорошо разбираешься в гаджетах и технике</li>
+// <li onClick={()=>selectTest(7)} className="menu-item">Знаешь ли ты игровую индустрию</li> */}
 
 function Menu() {
   const dispatch = useDispatch();
-  // const [currentTest, setCurrentTest] = React.useState(0);
   const selectTest = (id)=>{
     dispatch(setTestId(id))
   }
 
   return (
-    <>
+    <div>
       <h1>Выберите тест 💡</h1>
       <ul className="main-menu">
-        <li onClick={()=>selectTest(0)} className="menu-item">Тест на знание Frontend</li>
-        <li onClick={()=>selectTest(1)} className="menu-item">Тест на знание React</li>
-        <li onClick={()=>selectTest(2)} className="menu-item">Интересные вопросы про автомобили</li>
-        <li onClick={()=>selectTest(3)} className="menu-item">Насколько хорошо, ты знаешь кинематограф</li>
-        <li onClick={()=>selectTest(4)} className="menu-item">Тест на историю</li>
-        <li onClick={()=>selectTest(5)} className="menu-item">Тест по основам школьной физики</li>
-        <li onClick={()=>selectTest(6)} className="menu-item">Как хорошо разбираешься в гаджетах и технике</li>
-        <li onClick={()=>selectTest(7)} className="menu-item">Знаешь ли ты игровую индустрию</li>
+        {
+          tests.map(el => <li key={el.id} onClick={()=>selectTest(el.id)} className="menu-item">{el.title}</li>)
+        }
       </ul>
-    </>
+    </div>
   );
 }
 
-export default Menu
+export default Menu;
